c629e83380f126feb34ec98beef0160ccaf39dc8
sebnorth <sebnorth@gmail.com>
2020-11-13 12:39:26 +0100

    Dodaj pliki projektów.

---
+++ Lab7PR2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7PR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testowa = { 5, 13, 2, 25, 7, 17, 20, 8, 4 };
+            BinaryHeap kopiec = new BinaryHeap(testowa.Length);
+
+            for (int i = 0; i < testowa.Length; i++)
+            {
+                // kopiec.AddToHeap(testowa[i]);
+                kopiec.AddToArray(testowa[i]);
+            }
+
+            kopiec.WypiszKopiec();
+            Console.ReadKey();
+        }
+    }
+}
